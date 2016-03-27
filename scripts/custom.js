93f8dbbae57f84ddac080b9672a9fdfb98571338
prsolans <prsolans@gmail.com>
2016-03-27 15:50:29 -0500

    Added activities and dynamic drop downs

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,9 +1,29 @@
+var getCategories = function() {
+    $.getJSON("../data/categories.json", function(data) {
+        $.each(data["categories"], function() {
+            option = "<option value='" + this.name + "'>" + this.name + "</option>";
+            $("#selectCategory").append(option);
+        });
+    });
+};
+var getActivities = function(category) {
+    $.getJSON("../data/categories.json", function(data) {
+        $.each(data["categories"], function() {
+            if (this.name == category) {
+                activities = this.activities;
+                $.each(activities, function() {
+                    option = "<option class='dynamic' value='" + this.name + "'>" + this.name + "</option>";
+                    $("#selectActivity").append(option);
+                });
+            }
+        });
+    });
+};
 $(document).ready(function() {
-	console.log("one");
-	$.getJSON("../data/categories.json", function(data){
-		$.each( data, function(key, value) {
-			console.log(value);
-		});
-	});
-	console.log("two");
+    $("#selectCategory").on("change", function() {
+    	$("#selectActivity option.dynamic").remove();
+        var cat = $("#selectCategory").val();
+        getActivities(cat);
+    });
+    getCategories();
 });
