e3f651728b55d030faad1328186494eaf7eab68c
prsolans <prsolans@gmail.com>
2016-03-27 11:04:02 -0500

    Changed routing for JSON feed

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 	console.log("one");
-	$.getJSON("data/categories.json", function(data){
+	$.getJSON("../data/categories.json", function(data){
 		$.each( data, function(key, value) {
 			console.log(value);
 		});
 	});
+	console.log("two");
 });
