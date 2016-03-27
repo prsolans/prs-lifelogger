9caabdd1fee730ab3bdf4d985389d0ea48d2048f
prsolans <prsolans@gmail.com>
2016-03-27 18:38:18 -0500

    Query attempt

--- data/addActivity.php
+++ data/addActivity.php
@@ -1,3 +1,12 @@
 <?php
 	require 'connect.php';
+
+	$query = "INSERT INTO `prs-lifelogger-cleardb`.`activities` (`id`,`name`, `category`) VALUES (1, 'Run', 1);"
+
+	echo $query;
+
+	$result = mysql_query($query);
+	if (!$result) {
+	    die('Invalid query: ' . mysql_error());
+	}
 ?>
