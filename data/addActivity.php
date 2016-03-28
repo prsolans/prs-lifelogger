26dc935851502e80926796bae2d9596132badf2f
prsolans <prsolans@gmail.com>
2016-03-27 19:07:45 -0500

    Update query for auto-increment

--- data/addActivity.php
+++ data/addActivity.php
@@ -1,11 +1,7 @@
 <?php
 	require 'connect.php';
 
-	$activity = $_POST['activity'];
-
-	echo "ACT" . $activity;
-
-	$query = "INSERT INTO `prs-lifelogger-cleardb`.`activities` (`id`,`name`, `category`) VALUES (1, $activity, 1);";
+	$query = "INSERT INTO `prs-lifelogger-cleardb`.`activities` (`name`, `category`) VALUES ($_POST['activity'], 1);";
 
 	echo $query;
 	
