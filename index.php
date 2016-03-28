02a0afb84529c08e5cd918681bad880d60f24840
prsolans <prsolans@gmail.com>
2016-03-28 08:42:58 -0500

    reset to working status

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php 
 
-        require("classes/AltoRouter.php"); 
 
-        echo "1";
-
-        $router = new AltoRouter();
-
-        // map homepage using callable
-        $router->map( 'GET', '/', function() {
             require("/views/home.php");
-            echo "2";
-        });
+
 
 ?>
