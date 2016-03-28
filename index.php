da8f6fdcc2040f719ebeed613127edf1e9b30057
prsolans <prsolans@gmail.com>
2016-03-28 08:35:46 -0500

    exp7

--- index.php
+++ index.php
@@ -2,11 +2,14 @@
 
         require("classes/AltoRouter.php"); 
 
+        echo "1";
+
         $router = new AltoRouter();
 
         // map homepage using callable
         $router->map( 'GET', '/', function() {
             require("/views/home.php");
+            echo "2";
         });
 
 ?>
