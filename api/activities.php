44fde67cbb76037dd544957937bde4c632d24080
prsolans <prsolans@gmail.com>
2016-03-27 21:54:51 -0500

    Output results

--- api/activities.php
+++ api/activities.php
@@ -6,8 +6,14 @@
 	$query = "SELECT * FROM `prs-lifelogger-cleardb`.`activities` WHERE category='".$category."';";	
 
 	echo "<br>".$query;
-	$result = mysqli_query($conn,$query);
 
+	if (!$result = $conn->query($query)) {
+	    die ('There was an error running query[' . $conn->error . ']');
+	}
+
+    while ($row = $result->fetch_array()) {
+        echo 'ROW: '.$row["name"];
+    }
 	echo $result;
 
 	mysql_close($conn);
